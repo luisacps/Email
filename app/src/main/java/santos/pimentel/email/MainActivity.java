74bb0a256efd173b9b93ea2127d417e58e4e1b33
Luisa <luisacpsantos@gmail.com>
2024-04-02 10:58:51 -0300

    inicio da atividade de email

--- app/src/main/java/santos/pimentel/email/MainActivity.java
+++ app/src/main/java/santos/pimentel/email/MainActivity.java
@@ -3,6 +3,9 @@ package santos.pimentel.email;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // capturando o botao
+        Button btnEnviar = findViewById(R.id.btnEnviar);
+        // setando uma acao que ocorre com o clique no botao
+        btnEnviar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText etEmail = findViewById(R.id.etEmail);
+                String email = etEmail.getText().toString();
+
+                EditText etAssunto = findViewById(R.id.etAssunto);
+                String assunto = etAssunto.getText().toString();
+
+                EditText etTexto = findViewById(R.id.etTexto);
+                String texto = etTexto.getText().toString();
+            }
+        });
     }
 }
